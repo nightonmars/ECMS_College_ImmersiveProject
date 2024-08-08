eab1550d09ae21117dbd78cdd61293c6242695a3
nightonmars <139680225+nightonmars@users.noreply.github.com>
2024-08-08 19:25:59 +0100

    updates

---
+++ ECMS College Building/Assets/GlassDoorController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassDoorController : MonoBehaviour
+{
+
+    public Animator[] animators;
+    
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            foreach(Animator ani in animators)
+            {
+                ani.SetBool("OpenDoor", true); 
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (Animator ani in animators)
+            {
+                ani.SetBool("OpenDoor", false);
+            }
+        }
+    }
+
+
+}
